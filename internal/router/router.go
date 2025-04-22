9a3d7b0406bac5c6235c25faec1df9cd373a4ad8
Malyugin Anton <anton.malyugin@tabby.ai>
2025-04-22 20:54:17 +0200

    Iter7: Fix path for POST in router

--- internal/router/router.go
+++ internal/router/router.go
@@ -13,8 +13,8 @@ func SetupRoutes(s *storage.MemStorage) http.Handler {
 	handler := handlers.NewHandler(s)
 
 	r.Get("/", handler.GetMetrics)
-	r.Post("/value", handler.POSTGetMetric)
-	r.Post("/update", handler.POSTUpdateMetric)
+	r.Post("/value/", handler.POSTGetMetric)
+	r.Post("/update/", handler.POSTUpdateMetric)
 	r.Get("/value/{type}/{name}", handler.GETGetMetric)
 	r.Post("/update/{type}/{name}/{value}", handler.GETUpdateMetric)
 	return r
