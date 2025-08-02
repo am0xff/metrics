ab7f0342f04bcc34699f0169998d45e0f87edf36
Malyugin Anton <anton.malyugin@tabby.ai>
2025-08-02 14:17:52 +0200

    Iter20: Done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"github.com/am0xff/metrics/internal/server"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
 func main() {
+	printBuildInfo()
+
 	if err := server.Run(); err != nil {
 		panic(err)
 	}
+}
+
+func printBuildInfo() {
+	fmt.Printf("Build version: %s\n", getBuildValue(buildVersion))
+	fmt.Printf("Build date: %s\n", getBuildValue(buildDate))
+	fmt.Printf("Build commit: %s\n", getBuildValue(buildCommit))
+}
+
+func getBuildValue(value string) string {
+	if value == "" {
+		return "N/A"
+	}
+	return value
 }
